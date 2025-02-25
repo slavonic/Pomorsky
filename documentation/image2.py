09d441c310dfad3fbd61dd878315c7dcd1f72a54
Aleksandr Andreev <aleksandr.andreev@gmail.com>
2025-02-25 17:55:43 +0100

    Add images

--- documentation/image2.py
+++ documentation/image2.py
@@ -16,15 +16,15 @@ import argparse
 
 # Constants, these are the main "settings" for the image
 WIDTH, HEIGHT, MARGIN, FRAMES = 2048, 1024, 128, 1
-FONT_PATH = "fonts/ttf/RadioCanadaDisplay-Regular.ttf"
+FONT_PATH = "fonts/otf/Pomorsky-Regular.otf"
 FONT_LICENSE = "OFL v1.1"
 AUXILIARY_FONT = "Helvetica"
 AUXILIARY_FONT_SIZE = 48
 
-LINE_ONE = "ABCDEFGHIJKLMNOPQ"
-LINE_TWO = "RSTUVWXYZ123456789"
-LINE_THREE = "abcdefghijklmnopqrstu"
-LINE_FOUR = "vwxyz,.;:!@#$%^&*(){}[]"
+LINE_FOUR = " Ѳ Ѯ Ѱ Ѵ Ѿ "
+LINE_ONE = "А Б В Г Д Е Ж Ѕ З И І "
+LINE_TWO = " К Л М Н О Ѡ П Р С Т"
+LINE_THREE = "Ꙋ Ф Х Ц Ч Ш Щ Ѣ Ю Ѧ Ꙗ"
 BIG_TEXT_FONT_SIZE = 160
 BIG_TEXT_SIDE_MARGIN = MARGIN * 1
 BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5.45
